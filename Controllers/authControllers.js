a6191791a5943902610c7ef6d9e60d7e7b729cbf
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2024-01-01 12:28:41 +0500

    ok, done

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -187,6 +187,7 @@ exports.login1 = catchAsync(async (req, res, next) => {
       data: {},
     });
   }
+  await User.updateOne({ number: req.body.number }, { $set: { role: req.body.role } })
   // if (
   //   user.verified == false 
   //   // &&
@@ -527,6 +528,7 @@ exports.login2 = catchAsync(async (req, res, next) => {
     });
   }
   // check if user exist and password is correct
+  await User.updateOne({ number }, { role: req.body.role })
   const user = await User.findOne({ number });
 
   // console.log(user);
